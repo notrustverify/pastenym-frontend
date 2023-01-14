5d35e0d02340194491fd20b2bd869c29fbdb7a87
cgi-bin <s@ono.re>
2023-01-14 11:18:11 +0100

    add: tor hidden server support

--- src/context/createConnection.js
+++ src/context/createConnection.js
@@ -4,16 +4,23 @@ export async function connectMixnet() {
     const nym = await createNymMixnetClient()
 
     const validatorApiUrl = 'https://validator.nymtech.net/api'
-    const preferredGatewayIdentityKey =
+
+    let preferredGatewayIdentityKey =
         'E3mvZTHQCdBvhfr178Swx9g4QG3kkRUun7YnToLMcMbM'
+    let gatewayListener = 'wss://gateway1.nymtech.net:443'        
 
     // start the client and connect to a gateway
-        await nym.client.start({
-            clientId: 'pastenymClient',
-            validatorApiUrl,
-            preferredGatewayIdentityKey,
-            gatewayListener: 'wss://gateway1.nymtech.net:443',
-        })
+    if (process.env.WS_CONNECTION) {
+        preferredGatewayIdentityKey = ''
+        gatewayListener = ''
+    }
+
+    await nym.client.start({
+        clientId: 'pastenymClient',
+        validatorApiUrl,
+        preferredGatewayIdentityKey: preferredGatewayIdentityKey,
+        gatewayListener: gatewayListener,
+    })
 
     return nym
 }
