4c6fdf0e6ebc812008343a0a68673886276bdcff
cgi-bin/ <6095048+sven-hash@users.noreply.github.com>
2023-01-14 11:21:43 +0100

    Feat/tor support (#54)
    * small changes
    
    * add: tor hidden server support
    
    * add: tor hidden server support
    
    Co-authored-by: cgi-bin <s@ono.re>

--- src/context/createConnection.js
+++ src/context/createConnection.js
@@ -4,16 +4,26 @@ export async function connectMixnet() {
     const nym = await createNymMixnetClient()
 
     const validatorApiUrl = 'https://validator.nymtech.net/api'
-    const preferredGatewayIdentityKey =
+
+    let preferredGatewayIdentityKey =
         'E3mvZTHQCdBvhfr178Swx9g4QG3kkRUun7YnToLMcMbM'
 
+    // WSS is mandatory for HTTPS website
+    let gatewayListener = 'wss://gateway1.nymtech.net:443'
+
+    // give the possibility to not force the gateway to use. Can be useful for tor hidden service.
+    if (process.env.WS_CONNECTION) {
+        preferredGatewayIdentityKey = ''
+        gatewayListener = ''
+    }
+
     // start the client and connect to a gateway
-        await nym.client.start({
-            clientId: 'pastenymClient',
-            validatorApiUrl,
-            preferredGatewayIdentityKey,
-            gatewayListener: 'wss://gateway1.nymtech.net:443',
-        })
+    await nym.client.start({
+        clientId: 'pastenymClient',
+        validatorApiUrl,
+        preferredGatewayIdentityKey: preferredGatewayIdentityKey,
+        gatewayListener: gatewayListener,
+    })
 
     return nym
 }
